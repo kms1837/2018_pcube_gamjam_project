5584830771747ffcbebe60bdd7bd402596dc0971
busm9632 <ksm9051@gmail.com>
2018-01-18 17:52:40 +0900

    TimingSheep Resources Upload
    양세기 리소스 업로드

---
+++ Assets/scripts/TimingSheep_Manager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TimingSheep_Manager : MonoBehaviour {
+
+    public GameObject SheepPreFab;//불러올 양
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
