f01710d372abe5005c79eb3807f4cfef533bcea1
kms1837@gmail.com <kms1837@gmail.com>
2018-01-18 17:21:05 +0900

    added stop button from alarm clock scene

--- Assets/scripts/clock.cs
+++ Assets/scripts/clock.cs
@@ -5,45 +5,96 @@ using UnityEngine.UI;
 
 public class clock : MonoBehaviour {
     public Text ClockLabel;
+    public Text SecondLabel;
     public Text AccelLabel;
-    // Use this for initialization
 
     private int hour;
     private int minute;
     private int second;
 
     private float clockSpeed;
+    private bool secondVisualFlag;
 
-    private const float limitSpeed = 0.02f;
+    private float limit;
+
+    private Coroutine clockRoutine = null;
+
+    private const int startHour = 6;
+    private const int endHour = 9;
+    private const float startSpeed = 0.1f;
+    private const float accel = 0.02f;
+    private const float limitSecondSpeed = 0.001f;
+    private const float limitMinuteSpeed = 0.05f;
 
 	void Start () {
-        hour = 0;
+        hour = startHour;
         minute = 0;
         second = 0;
-        clockSpeed = 1.0f;
+        clockSpeed = startSpeed;
+        secondVisualFlag = false;
+
+        limit = limitSecondSpeed;
 
         InvokeRepeating("clockSpeedAccel", 1.0f, 1.0f);
-        StartCoroutine(clockCounter());
+        clockRoutine = StartCoroutine(clockCounter());
     }
 	
-	// Update is called once per frame
 	void Update () {
         string minuteStr = minute / 10 <= 0 ? "0" + minute.ToString() : minute.ToString();
         string hourStr = hour / 10 <= 0 ? "0" + hour.ToString() : hour.ToString();
+        string secondStr = second / 10 <= 0 ? "0" + second.ToString() : second.ToString();
+        
         ClockLabel.text = hourStr + ":" + minuteStr;
-
+        SecondLabel.text = "." + secondStr;
         AccelLabel.text = clockSpeed.ToString();
     }
 
     void clockSpeedAccel() {
-        clockSpeed = clockSpeed > limitSpeed ? clockSpeed - 0.05f : limitSpeed;
+        clockSpeed = clockSpeed > limit ? clockSpeed - accel : limit;
+    }
+
+    void secondVisual() {
+        second++;
+        second = second % 60;
+    }
+
+    void gameOver(){
+        Debug.Log("game over");
+        stopClock();
+
+        second = 0;
+    }
+
+    public void stopClock() {
+        Debug.Log("game Stop");
+        CancelInvoke("secondVisual");
+        StopCoroutine(clockRoutine);
     }
 
     IEnumerator clockCounter() {
         while ( true ) {
-            minute++;
+            if (!secondVisualFlag && clockSpeed >= limitSecondSpeed) {
+                second++;
+                minute = second >= 60 ? minute + 1 : minute;
+                second = second % 60;
+            } else {
+                if (!secondVisualFlag) {
+                    secondVisualFlag = true;
+                    clockSpeed = 0.2f;
+                    limit = limitMinuteSpeed;
+                    InvokeRepeating("secondVisual", limitSecondSpeed, limitSecondSpeed);
+                }
+                minute++;
+            }
+            
+            hour = minute >= 60 ? hour + 1 : hour;
+
             minute = minute % 60;
-            hour = minute == 0 ? hour + 1 : hour;
+
+            if (hour >= endHour) {
+                gameOver();
+                yield break;
+            }
 
             yield return new WaitForSeconds(clockSpeed);
         }
